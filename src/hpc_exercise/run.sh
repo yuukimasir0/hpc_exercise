b38dd6f5ac1046e38ab66b6c392f24c9b6014cc2
yuukimasir0 <86475595+yuukimasir0@users.noreply.github.com>
2023-11-24 16:42:20 +0900

    run.sh を更新

--- src/hpc_exercise/run.sh
+++ src/hpc_exercise/run.sh
@@ -6,7 +6,7 @@ for i in `seq 9 1 30`
 do
     for j in `seq 0 1 100`
     do
-        ./hpc_exercise $i >git > runtest/output_$i.txt
+        ./hpc_exercise $i >> runtest/output_$i.txt
         sleep 3
     done
 done
